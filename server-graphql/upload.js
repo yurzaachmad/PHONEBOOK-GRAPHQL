a0812ef89b86955656f1cf61241fd9ba3c8972ac
yurza <yurzaachmad@gmail.com>
2023-09-06 08:59:30 +0700

    last update

--- server-graphql/upload.js
+++ server-graphql/upload.js
@@ -4,18 +4,14 @@ var path = require("path");
 module.exports = {
   Upload: GraphQLUpload,
   processUpload: async (upload) => {
-    // Process the uploaded file here (e.g., save to storage, update database)
     const pictureName = `${Date.now()}-${picture.name}`;
     const uploadPath = path.join(__dirname, "public", "images", pictureName);
     picture.mv(uploadPath, async function (err) {
       if (err) return res.status(500).send(err);
-      // Save the file path or identifier in the database (assuming 'picturePath' is the field name)
       const user = await models.User.update(
         {
           picturePath: uploadPath,
           avatar: pictureName,
-          // If using an identifier instead of file path, use something like:
-          // pictureIdentifier: pictureName,
         },
         {
           where: {
@@ -25,8 +21,7 @@ module.exports = {
           plain: true,
         }
       );
-      // Send the picture path or identifier in the response
-      res.json(user[1]); // Or 'user[1].pictureIdentifier' if using an identifier
+      res.json(user[1]);
     });
   },
 };
